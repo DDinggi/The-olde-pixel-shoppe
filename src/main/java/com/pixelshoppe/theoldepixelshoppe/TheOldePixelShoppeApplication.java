f73c8a5a99345c3cb3171c1be7c632fc848f1015
명기 조 <whaudrl123@naver.com>
2025-04-22 19:06:35 +0900

    Initial commit: Basic Spring Boot setup

---
+++ src/main/java/com/pixelshoppe/theoldepixelshoppe/TheOldePixelShoppeApplication.java
@@ -0,0 +1,13 @@
+package com.pixelshoppe.theoldepixelshoppe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheOldePixelShoppeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheOldePixelShoppeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pixelshoppe/theoldepixelshoppe/TheOldePixelShoppeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelshoppe.theoldepixelshoppe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheOldePixelShoppeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
